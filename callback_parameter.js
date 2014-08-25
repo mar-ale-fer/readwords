8a7588fb291d8c1eb2df4428ebef44c386c56a69
Marcelo Alejandro Fernández <maralefer@outlook.com.ar>
2014-08-25 18:03:25 -0300

    ajuste en callback_parameter

--- callback_parameter.js
+++ callback_parameter.js
@@ -1,20 +1,23 @@
 var events = require('events');
 function CarShow() {
   events.EventEmitter.call(this);
-  this.seeCar = function(make) {
-    this.emit('sawCar', make);
+  this.seeCar = function(car) {
+    this.emit('sawCar', car);
   };
 }
 CarShow.prototype.__proto__ = events.EventEmitter.prototype;
 var show = new CarShow();
-function logCar(make) {
-  console.log("Saw a " + make);
+function logCar(car) {
+  console.log("Saw a " + car);
+}
+function logColorCar(car, color) {
+  console.log("Saw a %s %s", color, car);
 }
 show.on("sawCar", logCar);
-show.on("sawCar", function(make){
+show.on("sawCar", function(car){
   var colors = ['red','blue','black'];
   var color = colors[Math.floor(Math.random()*3)];
-  logColorCar(make, color);
+  logColorCar(car, color);
 });
 show.seeCar("Ferrari");
 show.seeCar("Bugatti");
