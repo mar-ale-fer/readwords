926b4b55fce7ab582be030ad44cd606dce31e8ea
Marcelo Alejandro Fernández <maralefer@outlook.com.ar>
2014-08-29 12:33:00 -0300

    Revert "ajuste en callback_parameter"
    This reverts commit 8a7588fb291d8c1eb2df4428ebef44c386c56a69.

--- callback_parameter.js
+++ callback_parameter.js
@@ -1,23 +1,20 @@
 var events = require('events');
 function CarShow() {
   events.EventEmitter.call(this);
-  this.seeCar = function(car) {
-    this.emit('sawCar', car);
+  this.seeCar = function(make) {
+    this.emit('sawCar', make);
   };
 }
 CarShow.prototype.__proto__ = events.EventEmitter.prototype;
 var show = new CarShow();
-function logCar(car) {
-  console.log("Saw a " + car);
-}
-function logColorCar(car, color) {
-  console.log("Saw a %s %s", color, car);
+function logCar(make) {
+  console.log("Saw a " + make);
 }
 show.on("sawCar", logCar);
-show.on("sawCar", function(car){
+show.on("sawCar", function(make){
   var colors = ['red','blue','black'];
   var color = colors[Math.floor(Math.random()*3)];
-  logColorCar(car, color);
+  logColorCar(make, color);
 });
 show.seeCar("Ferrari");
 show.seeCar("Bugatti");
