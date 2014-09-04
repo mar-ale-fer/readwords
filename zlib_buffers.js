ba443db2e89a6579a12f5aca059fb1ce9c3efa77
mar-ale-fer <maralefer@outlook.com.ar>
2014-09-04 00:39:29 -0400

    zlib completo

--- zlib_buffers.js
+++ zlib_buffers.js
@@ -1,5 +1,5 @@
 var zlib = require("zlib");
-var input = '...............text__________----=';
+var input = '10000000000000000000000000000000000000000000000000000000001111111111111111.........................';
 zlib.deflate(input, function(err, buffer) {
   if (!err) {
 	//en buffer recibe input comprimido
@@ -18,4 +18,30 @@ zlib.deflate(input, function(err, buffer) {
 	});
   }
 });
-	
+
+zlib.deflateRaw(input, function(err, buffer) {
+  if (!err) {
+    console.log("deflateRaw (%s): ", buffer.length, buffer.toString('base64'));
+	zlib.inflateRaw(buffer, function(err, buffer) {
+	  if (!err) {
+	    console.log("inflateRaw (%s): ", buffer.length, buffer.toString());
+      }
+	});
+  }
+});
+
+zlib.gzip(input, function(err, buffer) {
+  if (!err) {
+    console.log("gzip (%s): ", buffer.length, buffer.toString('base64'));
+    zlib.gunzip(buffer, function(err, buffer) {
+      if (!err) {
+        console.log("gunzip (%s): ", buffer.length, buffer.toString());
+      }
+    });
+    zlib.unzip(buffer, function(err, buffer) {
+      if (!err) {
+        console.log("unzip gzip (%s): ", buffer.length, buffer.toString());
+      }
+    });
+  }
+});  
