d12885ebca23f8ed8fbc37d99d22de2129165e30
Marcelo Alejandro Fernández <maralefer@outlook.com.ar>
2014-09-08 17:46:58 -0300

    file_write

---
+++ file_write.js
@@ -0,0 +1,16 @@
+//pos 2538 del libro
+var fs = require('fs');
+var config = {
+  maxFiles: 20,
+  maxConnections: 15,
+  rootPath: "/webroot"
+};
+var configTxt = JSON.stringify(config);
+var options = {encoding:'utf8', flag:'w'};
+fs.writeFile('config.txt', configTxt, options, function(err) {
+  if (err) {
+    console.log("Config Write Failed.");
+  } else {
+    console.log("Config saved.");
+  }
+});
